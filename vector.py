713be37ca9d493972cf3136682f0790081d5dca0
David <dcaulfie@stetson.edu>
2017-04-08 11:59:32 -0400

    Created normalized and magnitude functions

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+from math import sqrt 
+
 class Vector(object):
 	def __init__(self, coordinates):
 		try:
@@ -24,10 +26,25 @@ class Vector(object):
 		new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
 		return Vector(new_coordinates)
 
-		def plus(self, v):
+	def plus(self, v):
 		new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
 		return Vector(new_coordinates)
 
 	def times_scalar(self, c):
 		new_coordinates = [c*x for x in self.coordinates]
 		return Vector(new_coordinates)
+
+	def magnitude(self):
+		coordinates_squared = [x**2 for x in self.coordinates]
+		return sqrt(sum(coordinates_squared))
+
+	def normalized(self):
+		try:
+			magniatude = self.magniatude()
+			return self.times_scalar(1/magniatude)
+		except ZeroDivisionError:
+			raise Exception('Cannot normalize the zero vector')
+
+a = Vector([-0.221, 7.437])
+print(a.magniatude())
+print(a.__eq__())
